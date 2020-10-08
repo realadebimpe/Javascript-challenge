c4a361c2eaa9e7cfcf582789c3763cfb5ba7f0d4
Adebimpe <realadebimpe@gmail.com>
2020-10-08 18:31:30 +0100

    Added html and modified js

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1st challenge
+
+function exerciseOne(){
+    // Create a variable named 'myName' and assign it a string containing your name
+    var myName = "Adebimpe";
+    // Please write answer above  
+      return myName
+    }
+    function exerciseTwo(){
+    // Create a variable named 'tripsAroundTheSun' and assign your age as a number:
+    var tripsAroundTheSun = 26;
+    // Please write answer above  
+      return tripsAroundTheSun
+    }
+    function exerciseThree(){
+    //Create a variable named 'pineappleOnPizza' and assign it a Boolean of whether or not you think pineapple belongs on pizza.
+    var pineappleOnPizza = false;
+    // Please write answer above 
+      return pineappleOnPizza
+    }
+
+    console.log(exerciseOne);
+    console.log(exerciseTwo);
+    console.log(exerciseThree);
+
+    // 2nd chllenge
+    //Write a function that takes an array of strings and return the longest string in the array.
+
+    const arr = ["Having", "fun", "with", "javascript"]
+    function longestString (arr) {
+      for (var i = 0; i <arr.length; i++){
+         
+      }
+      return arr[3];
+    }
+    console.log(myArr)
